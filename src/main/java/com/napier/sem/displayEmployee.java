fe61f6f745765f7f0c93c7cac069d4d791c630bc
Shandi King <shandiking@hotmail.com>
2021-02-22 15:02:07 -0500

    Employee java

---
+++ src/main/java/com/napier/sem/displayEmployee.java
@@ -0,0 +1,18 @@
+package com.napier.sem;
+
+public class displayEmployee {
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
+}
